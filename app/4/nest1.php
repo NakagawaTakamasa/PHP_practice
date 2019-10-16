8db62e69f1bab1198bae7eeb08dd2ba8ce87aa55
Mac PC02 <nakagawa-takamasa@Mac-PC02.local>
2019-10-16 23:54:06 +0900

    10/16　php 進行分

---
+++ app/4/nest1.php
@@ -0,0 +1,16 @@
+<?php
+
+$attend = 1;
+//欠席は0　出席は1
+$place = chr(mt_rand(65,66));
+
+if($attend === 0){
+  echo 'パーティーは欠席にて承りました。<br>';
+}else{
+  echo 'パーティーは出席にて承りました。<br>';
+  if($place === 'A'){
+    echo '会場はAホテルでございます。';
+  }elseif($place === 'B'){
+    echo '会場はB広場でございます。';
+  }
+}
---
+++ app/4/return.php
@@ -0,0 +1,6 @@
+<?php
+
+$num = 5;
+var_dump($num < 3);
+
+?>
