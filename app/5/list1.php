3af7000f2056123177251229292e8bbbf6f7ba1d
Nakagawa Takamasa <t.nakagawa0220@gmail.com>
2019-10-18 21:33:31 +0900

    10/18 コードクリエイト

---
+++ app/5/list1.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<style>
+ul{width:100px;}
+.color-red{background-color:red;}
+</style>
+</head>
+<body>
+<ul>
+    <?php for ($i = 1; $i <= 20; $i++) {
+     //
+        if($i % 2 === 1){ ?>
+
+        }
+    } ?>
+</ul>
+
+</body>
+</html>
