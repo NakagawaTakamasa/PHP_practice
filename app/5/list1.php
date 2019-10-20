ab9740eaf4af69579b01985c6f4abb2f25965b7b
Nakagawa Takamasa <t.nakagawa0220@gmail.com>
2019-10-20 18:51:19 +0900

    10/20 php参考書進行

--- app/5/list1.php
+++ app/5/list1.php
@@ -7,12 +7,14 @@ ul{width:100px;}
 </head>
 <body>
 <ul>
-    <?php for ($i = 1; $i <= 20; $i++) {
+    <?php for ($i = 1; $i <= 100; $i++) {
      //
-        if($i % 2 === 1){ ?>
-
-        }
-    } ?>
+        if($i % 2 === 0){ ?>
+        <li class = "color-red"><?php echo $i;?></li>
+        <?php }else{ ?>
+        <li><?php echo $i;?></li>
+        <?php }
+        } ?>
 </ul>
 
 </body>
